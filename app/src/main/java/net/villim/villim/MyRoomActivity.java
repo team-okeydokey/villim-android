1f24b66eece08dba29653c6a23a331fc5bf3f501
Seongmin Park <seongmin@Seongmins-iMac.local>
2017-05-07 18:38:17 -0400

    Add glide and prugio thumbnail

--- app/src/main/java/net/villim/villim/MyRoomActivity.java
+++ app/src/main/java/net/villim/villim/MyRoomActivity.java
@@ -2,11 +2,15 @@ package net.villim.villim;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class MyRoomActivity extends AppCompatActivity {
 
     private TextView myroomTitle;
+    private ImageView myroomThumbnail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +18,13 @@ public class MyRoomActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_room);
 
         myroomTitle = (TextView) findViewById(R.id.myroom_title);
+        myroomThumbnail = (ImageView) findViewById(R.id.myroom_thumbnail);
 
+        loadThumbnail();
+    }
+
+    private void loadThumbnail() {
+        Glide.with(this).load(R.drawable.prugio_thumbnail).into(myroomThumbnail);
     }
 
 
