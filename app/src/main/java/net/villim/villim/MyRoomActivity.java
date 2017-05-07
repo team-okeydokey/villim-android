2ae85c992de396f1cd8410b64fb570b92a272d82
Seongmin Park <seongmin@Seongmins-iMac.local>
2017-05-07 18:57:46 -0400

    First attempt at bottom line

--- app/src/main/java/net/villim/villim/MyRoomActivity.java
+++ app/src/main/java/net/villim/villim/MyRoomActivity.java
@@ -2,6 +2,7 @@ package net.villim.villim;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -9,6 +10,7 @@ import com.bumptech.glide.Glide;
 
 public class MyRoomActivity extends AppCompatActivity {
 
+    private LinearLayoutCompat mainLayout;
     private TextView myroomTitle;
     private ImageView myroomThumbnail;
 
@@ -17,14 +19,24 @@ public class MyRoomActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_room);
 
+        mainLayout = (LinearLayoutCompat) findViewById(R.id.main_layout);
         myroomTitle = (TextView) findViewById(R.id.myroom_title);
         myroomThumbnail = (ImageView) findViewById(R.id.myroom_thumbnail);
 
-        loadThumbnail();
+        populateView();
     }
 
-    private void loadThumbnail() {
+    private void populateView() {
+        // Fetch and insert room thumbnail.
         Glide.with(this).load(R.drawable.prugio_thumbnail).into(myroomThumbnail);
+
+        // Draw line.
+        LineView bottomLine = new LineView(getApplicationContext(), 0,600,300,600, 0);
+        bottomLine.setLayoutParams(new LinearLayoutCompat.LayoutParams(
+                LinearLayoutCompat.LayoutParams.MATCH_PARENT,
+                LinearLayoutCompat.LayoutParams.MATCH_PARENT));
+
+        mainLayout.addView(bottomLine);
     }
 
 
