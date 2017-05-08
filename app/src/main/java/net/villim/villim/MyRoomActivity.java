0de1c24b74458564a084120a6cbc93a9508e1eb6
Seongmin Park <seongmin@Seongmins-iMac.local>
2017-05-07 23:56:57 -0400

    Finish prototype myroom activity

--- app/src/main/java/net/villim/villim/MyRoomActivity.java
+++ app/src/main/java/net/villim/villim/MyRoomActivity.java
@@ -11,6 +11,12 @@ public class MyRoomActivity extends AppCompatActivity {
 
     private TextView myroomTitle;
     private TextView myroomRoomName;
+
+    private TextView myroomNumGuest;
+    private TextView myroomRoomType;
+    private TextView myroomNumBed;
+    private TextView myroomNumBath;
+
     private TextView myroomRoomDuration;
     private ImageView myroomThumbnail;
 
@@ -20,8 +26,19 @@ public class MyRoomActivity extends AppCompatActivity {
         setContentView(R.layout.activity_my_room);
 
         myroomTitle = (TextView) findViewById(R.id.myroom_title);
+
+        // Room name.
         myroomRoomName = (TextView) findViewById(R.id.myroom_roomname);
+
+        // Room info.
+        myroomNumGuest = (TextView) findViewById(R.id.myroom_numguest);
+        myroomRoomType = (TextView) findViewById(R.id.myroom_roomtype);
+        myroomNumBed = (TextView) findViewById(R.id.myroom_numbed);
+        myroomNumBath = (TextView) findViewById(R.id.myroom_numbath);
+
+        // Room duration.
         myroomRoomDuration = (TextView) findViewById(R.id.myroom_roomduration);
+
         myroomThumbnail = (ImageView) findViewById(R.id.myroom_thumbnail);
 
         populateView();
@@ -34,7 +51,13 @@ public class MyRoomActivity extends AppCompatActivity {
         // Populate room name.
         myroomRoomName.setText("Prugio Studio");
 
-        // Populate room name.
+        // Populate room information.
+        myroomNumGuest.setText("Guest 1");
+        myroomRoomType.setText("Studio");
+        myroomNumBed.setText("Bed 1");
+        myroomNumBath.setText("Bath 1");
+
+        // Populate room duration.
         myroomRoomDuration.setText("Duration of stay: 2017/04/16 ~ 06/19");
 
         // Fetch and insert room thumbnail.
