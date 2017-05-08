1578f3fa5cabaff0a004a10fb485baf315f149da
Seongmin Park <seongmin@Seongmins-iMac.local>
2017-05-07 23:07:41 -0400

    Apply card layout

--- app/src/main/java/net/villim/villim/MyRoomActivity.java
+++ app/src/main/java/net/villim/villim/MyRoomActivity.java
@@ -2,7 +2,6 @@ package net.villim.villim;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,8 +9,9 @@ import com.bumptech.glide.Glide;
 
 public class MyRoomActivity extends AppCompatActivity {
 
-    private LinearLayoutCompat mainLayout;
     private TextView myroomTitle;
+    private TextView myroomRoomName;
+    private TextView myroomRoomDuration;
     private ImageView myroomThumbnail;
 
     @Override
@@ -19,16 +19,28 @@ public class MyRoomActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_room);
 
-        mainLayout = (LinearLayoutCompat) findViewById(R.id.main_layout);
         myroomTitle = (TextView) findViewById(R.id.myroom_title);
+        myroomRoomName = (TextView) findViewById(R.id.myroom_roomname);
+        myroomRoomDuration = (TextView) findViewById(R.id.myroom_roomduration);
         myroomThumbnail = (ImageView) findViewById(R.id.myroom_thumbnail);
 
         populateView();
     }
 
+    // Make this async.
     private void populateView() {
+        // Network operation to fetch.
+
+        // Populate room name.
+        myroomRoomName.setText("Prugio Studio");
+
+        // Populate room name.
+        myroomRoomDuration.setText("Duration of stay: 2017/04/16 ~ 06/19");
+
         // Fetch and insert room thumbnail.
-        Glide.with(this).load(R.drawable.prugio_thumbnail).into(myroomThumbnail);
+        Glide.with(this)
+                .load(R.drawable.prugio_thumbnail)
+                .into(myroomThumbnail);
     }
 
 
