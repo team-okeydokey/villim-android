a9a2dcf1303dfd1e96eb959886438a5aae35a807
Seongmin Park <seongminnpark@gmail.com>
2017-05-23 17:34:26 -0400

    Finish Profile page

--- app/src/main/java/net/villim/villim/ProfileFragment.java
+++ app/src/main/java/net/villim/villim/ProfileFragment.java
@@ -1,6 +1,7 @@
 package net.villim.villim;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewCompat;
@@ -10,8 +11,11 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+
+import java.util.List;
 
 
 public class ProfileFragment extends Fragment {
@@ -32,12 +36,12 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View profileView = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        // Remove bottom bar.
         activity = ((MainActivity) getActivity());
         activity.setBottomButton(false, null);
 
         profileListView = (ListView) profileView.findViewById(R.id.profile_listView);
         profileItems = getResources().getStringArray(R.array.profile_items);
-        profileListView.setAdapter(new ArrayAdapter<String>(activity, R.layout.profile_list_item, R.id.profile_list_item_title, profileItems));
 
         // Set title.
         activity.setTitle(getString(R.string.profile_title));
@@ -59,13 +63,35 @@ public class ProfileFragment extends Fragment {
     private void populateView() {
         // Network operation to fetch.
 
+        // Populate info.
+        profileListView.setAdapter(new ProfileAdapter(activity, R.layout.profile_list_item, R.id.profile_list_item_title, profileItems));
+
         // Fetch and insert room thumbnail.
         Glide.with(this)
                 .load(R.drawable.prugio_thumbnail)
                 .into(profilePicture);
+    }
 
-        // Remove bottom bar.
-        activity.setBottomButton(false, null);
+
+    // Used to populate profile info list layout.
+    private class ProfileAdapter extends ArrayAdapter<String> {
+
+        public ProfileAdapter(Context context, int resource, int textViewResourceId, String[] objects) {
+            super(context, resource, textViewResourceId, objects);
+        }
+
+        @Override
+        public int getCount() {
+            return super.getCount();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View itemView = super.getView(position, convertView, parent);
+            TextView itemValueText = (TextView) itemView.findViewById(R.id.profile_list_item_value);
+            itemValueText.setText("이름이메일전번비번");
+            return itemView;
+        }
     }
 
 }
