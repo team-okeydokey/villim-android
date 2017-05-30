1ef4f35787be79cb370351baab66d22b16740568
Seongmin Park <seongminnpark@gmail.com>
2017-05-30 18:23:14 -0400

    Make adaptive back button

--- app/src/main/java/net/villim/villim/RoomDetailActivity.java
+++ app/src/main/java/net/villim/villim/RoomDetailActivity.java
@@ -1,15 +1,23 @@
 package net.villim.villim;
 
+import android.graphics.PorterDuff;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+
 public class RoomDetailActivity extends AppCompatActivity {
 
+    private AppBarLayout appBarLayout;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
     private Toolbar toolbar;
     private ImageView toolbarImage;
 
@@ -27,6 +35,24 @@ public class RoomDetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(ContextCompat.getDrawable(getApplicationContext(), R.drawable.back_arrow_light));
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        /* Change back button color on collapse */
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        appBarLayout = (AppBarLayout) findViewById(R.id.app_bar_layout);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if ((collapsingToolbarLayout.getHeight() + verticalOffset) < (2 * ViewCompat.getMinimumHeight(collapsingToolbarLayout))) {
+                    toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.arrow_dark), PorterDuff.Mode.SRC_ATOP);
+
+                } else {
+                    toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.arrow_light), PorterDuff.Mode.SRC_ATOP);
+                }
+            }
+        });
+
+        TextView tv = (TextView) findViewById(R.id.text_view);
+//        tv.setNestedScrollingEnabled(false);
 
         populateView();
 
