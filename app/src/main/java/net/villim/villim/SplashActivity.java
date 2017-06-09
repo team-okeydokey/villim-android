948ec4601a025f0df328179cbf1e9e7c0dc76a3f
Seongmin Park <seongminnpark@gmail.com>
2017-06-09 12:12:45 -0400

    Add splash screen and initialize mapview at launch

---
+++ app/src/main/java/net/villim/villim/SplashActivity.java
@@ -0,0 +1,35 @@
+package net.villim.villim;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.MapView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /* Initialize mapview & Google Play Services */
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    MapView mv = new MapView(getApplicationContext());
+                    mv.onCreate(null);
+                    mv.onPause();
+                    mv.onDestroy();
+                }catch (Exception ignored){
+
+                }
+            }
+        }).start();
+
+        /* Launch MainActivity */
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
