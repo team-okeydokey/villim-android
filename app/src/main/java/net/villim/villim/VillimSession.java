9fef717b87d39801aaa98e9092afbeb0ed975da7
Seongmin Park <seongminnpark@gmail.com>
2017-06-09 21:17:54 -0400

    VillimSession

---
+++ app/src/main/java/net/villim/villim/VillimSession.java
@@ -0,0 +1,40 @@
+package net.villim.villim;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by seongmin on 6/9/17.
+ */
+
+public class VillimSession {
+    public final static String KEY_LOGGED_IN = "logged_in";
+    public final static String KEY_NAME = "name";
+
+    private SharedPreferences prefs;
+
+    public VillimSession(Context context) {
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        if (!loggedIn) {
+            prefs.edit().clear().apply();
+        }
+        prefs.edit().putBoolean(KEY_LOGGED_IN, loggedIn);
+    }
+
+    public void getLoggedIn() {
+        return  prefs.getBoolean(KEY_LOGGED_IN,"");
+    }
+
+    public void setName(String usename) {
+        prefs.edit().putString(KEY_NAME, usename).apply();
+    }
+
+    public String getName() {
+        String usename = prefs.getString(KEY_NAME,"");
+        return usename;
+    }
+}
