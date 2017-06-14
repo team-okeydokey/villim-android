f7a134fdc8bb579923f31555c780da40f41aa4eb
Seongmin Park <seongminnpark@gmail.com>
2017-06-14 15:35:39 -0400

    Lay views out in review activity

--- app/src/main/java/net/villim/villim/ViewReviewActivity.java
+++ app/src/main/java/net/villim/villim/ViewReviewActivity.java
@@ -5,11 +5,23 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class ViewReviewActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private Button closeButton;
+
+    private TextView reviewCount;
+
+    private RatingBar overAllRatingBar;
+    private RatingBar accuracyRatingbar;
+    private RatingBar communicationRatingBar;
+    private RatingBar cleanlinessRatingBar;
+    private RatingBar locationRatingBar;
+    private RatingBar checkinRatingBar;
+    private RatingBar valueRatingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +43,23 @@ public class ViewReviewActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        /* Title */
+        reviewCount = (TextView) findViewById(R.id.review_count);
+
+        /* Rating bars */
+        overAllRatingBar = (RatingBar) findViewById(R.id.overall_ratingbar);
+        accuracyRatingbar = (RatingBar) findViewById(R.id.accuracy_ratingbar);
+        communicationRatingBar = (RatingBar) findViewById(R.id.communication_ratingbar);
+        cleanlinessRatingBar = (RatingBar) findViewById(R.id.cleanliness_ratingbar);
+        locationRatingBar = (RatingBar) findViewById(R.id.location_ratingbar);
+        checkinRatingBar = (RatingBar) findViewById(R.id.checkin_ratingbar);
+        valueRatingBar = (RatingBar) findViewById(R.id.value_ratingbar);
+
+        sendGetHouseReviewRequest();
+    }
+
+    private void sendGetHouseReviewRequest() {
+        
     }
 }
