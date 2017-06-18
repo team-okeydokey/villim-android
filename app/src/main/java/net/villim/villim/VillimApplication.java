58622557aabfa931f3481eebd490a2d8c2a3a39f
Seongmin Park <seongminnpark@gmail.com>
2017-06-17 22:20:28 -0400

    Add calligraphy library

---
+++ app/src/main/java/net/villim/villim/VillimApplication.java
@@ -0,0 +1,22 @@
+package net.villim.villim;
+
+import android.app.Application;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+/**
+ * Created by seongmin on 6/17/17.
+ */
+
+public class VillimApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/NotoSansCJKkr-Regular.otf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+    }
+}
